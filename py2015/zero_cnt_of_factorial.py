995063484de418ce7201d10c829fc26cec725984
BreakTom <esapinit@163.com>
2016-03-10 10:40:32 +0800

    Update zero_cnt_of_factorial.py
    i believe it's better

--- py2015/zero_cnt_of_factorial.py
+++ py2015/zero_cnt_of_factorial.py
@@ -3,20 +3,15 @@ calculate how many zero(0)s in the end of n!.
 '''
 
 
-def zero_cnt_of_factorial(num=50):
+def zero_cnt_of_factorial(num):
     count = 0
-    delta = 5
-    m = 5
-    while m <= num:
-        count += 1
-        m1 = m / delta
-        while m1 > 0:
-            if m1 % delta == 0:
-                count += 1
-            m1 = m1 / delta
-        m += 5
+    
+    while num/5:
+        count += num/5
+        num /= 5
+        
     return count
 
 if __name__ == '__main__':
     n = int(raw_input('Enter a number: '))
-    print "%d! has %d zeros in the end." % (n, zero_cnt_of_factorial(n))
+    print "{0} has {1} zeros in the end.".format(n, zero_cnt_of_factorial(n))
